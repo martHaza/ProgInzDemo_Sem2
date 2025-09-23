ff0b1d9816371ed9421941ca3801fc089bc9ea6e
martHaza <martazapacka17@gmail.com>
2025-09-23 20:51:29 +0300

    Pievienots MyAuthority un MyUser

--- src/main/java/lv/venta/ProgInzDemoSem2Application.java
+++ src/main/java/lv/venta/ProgInzDemoSem2Application.java
@@ -7,14 +7,20 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import lv.venta.model.Course;
-import lv.venta.model.enums.Degree;
 import lv.venta.model.Grade;
+import lv.venta.model.MyAuthority;
+import lv.venta.model.MyUser;
 import lv.venta.model.Professor;
 import lv.venta.model.Student;
+import lv.venta.model.enums.Degree;
 import lv.venta.repo.ICourseRepo;
 import lv.venta.repo.IGradeRepo;
+import lv.venta.repo.IMyAuthorityRepo;
+import lv.venta.repo.IMyUserRepo;
 import lv.venta.repo.IProfessorRepo;
 import lv.venta.repo.IStudentRepo;
 
@@ -26,22 +32,23 @@ public class ProgInzDemoSem2Application {
 	}
 	@Bean
 	public CommandLineRunner testModelLayer(IStudentRepo studRepo, 
-			IProfessorRepo profRepo, IGradeRepo grRepo, ICourseRepo courseRepo)
+			IProfessorRepo profRepo, IGradeRepo grRepo, ICourseRepo courseRepo ,
+			IMyAuthorityRepo authRepo, IMyUserRepo userRepo)
 	{
 		return new CommandLineRunner() {
 			
 			@Override
 			public void run(String... args) throws Exception {
-				Student s1 = new Student("Konstantins", "Cmils");
+				Student s1 = new Student("Konstantīns", "Čmils");
 				Student s2 = new Student("Guna", "Kravale");
-				Student s3 = new Student("Sintija", "Ernstreite");
+				Student s3 = new Student("Sintija", "Ernštreite");
 				
 				studRepo.save(s1);
 				studRepo.save(s2);
 				studRepo.save(s3);
 				
-				Professor p1 = new Professor("Karina", "Skirmante", Degree.magistra);
-				Professor p2 = new Professor("Karlis", "Immers", Degree.magistra);
+				Professor p1 = new Professor("Karina", "Šķirmante", Degree.magistra);
+				Professor p2 = new Professor("Kārlis", "Immers", Degree.magistra);
 				Professor p3 = new Professor("Vija", "Vagale", Degree.doktora);
 				
 				//garais pieraksts
@@ -53,9 +60,9 @@ public class ProgInzDemoSem2Application {
 				//īsais pieraksts
 				profRepo.saveAll((Arrays.asList(p1, p2, p3)));
 			
-				Course c1 = new Course("Programmaturas Inzenierija I", 6, p1);
-				Course c2 = new Course("Datubazes II", 3, p3);
-				Course c3 = new Course("Web tehnologijas", 6, p2);
+				Course c1 = new Course("Programmatūras Inženierija I", 6, p1);
+				Course c2 = new Course("Datubāzes II", 3, p3);
+				Course c3 = new Course("Web tehnoloģijas", 6, p2);
 				
 				courseRepo.saveAll(Arrays.asList(c1, c2, c3));
 				
@@ -67,6 +74,24 @@ public class ProgInzDemoSem2Application {
 				Grade g6 = new Grade(7, s3, c2);//Sintija nopelnīja 7 Datubāzēs II
 				Grade g7 = new Grade(5, s3, c3);//Sintija nopelnīja 5 Webteh
 				grRepo.saveAll(Arrays.asList(g1, g2, g3, g4, g5, g6, g7));
+				
+				
+				MyAuthority auth1 = new MyAuthority("USER");
+				authRepo.save(auth1);
+				
+				MyAuthority auth2 = new MyAuthority("ADMIN");
+				authRepo.save(auth2);	
+				
+				PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+				
+				MyUser u1 = new MyUser("karina", encoder.encode("123"));
+				userRepo.save(u1);
+				
+				MyUser u2 = new MyUser("janis", encoder.encode("321"));
+				userRepo.save(u2);			
+				
+				//TODO izveidot sasaisti starp MyAuthority un MyUser klasēm 
+				
 			}
 		};
 	}
