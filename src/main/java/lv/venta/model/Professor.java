51e14bc84597178637f305ddc5a1cc8f657017f3
martHaza <martazapacka17@gmail.com>
2025-09-26 14:49:01 +0300

    Pievienots @JsonIgnore

--- src/main/java/lv/venta/model/Professor.java
+++ src/main/java/lv/venta/model/Professor.java
@@ -1,5 +1,7 @@
 package lv.venta.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -50,6 +52,7 @@ public class Professor {
 	
 	@OneToOne(mappedBy = "professor")//nodrosinām sasaisti JAVA līmenī - otras klases mainīgā nosaukums
 	@ToString.Exclude
+	@JsonIgnore
 	private Course course;
 	
 	
